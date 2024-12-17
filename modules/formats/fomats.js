e47f52f9639b6dddb8b760821e240124b208abc8
Dejan NenaDovic <n.dejanfl@gmail.com>
2024-12-17 05:45:33 +0100

    Update fomats.js

--- modules/formats/fomats.js
+++ modules/formats/fomats.js
@@ -1,15 +1,4 @@
-export function formatNumber(number) {
-    if (number >= 1e9) {
-      return (number / 1e9).toFixed(2) + "B";
-    }
-    if (number >= 1e6) {
-      return (number / 1e6).toFixed(2) + "M";
-    }
-    if (number >= 1e3) {
-      return (number / 1e3).toFixed(2) + "K";
-    }
-    return number.toFixed(2);
-  }
+
   
   export function formatElapsedTime(createdDate) {
     const now = Date.now();
